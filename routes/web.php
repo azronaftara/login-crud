30b2dded136e1ad098b115fe5ba661ac7fb84d1d
azronaftara <azronaftara@gmail.com>
2025-02-25 16:34:37 +0700

    sampai function submit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiswaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/siswa', [SiswaController::class, 'tampil'])->name('siswa.tampil');
+Route::get('/siswa/tambah', [SiswaController::class, 'tambah'])->name('siswa.tambah');
+Route::post('/siswa/submit', [SiswaController::class, 'submit'])->name('siswa.submit');
